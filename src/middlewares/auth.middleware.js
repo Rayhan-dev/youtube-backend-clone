ae82658a9b6cb6b9e87d4410693f03c8aed49dea
Mohammod Rayhan <mohammodrayhan444@gmail.com>
2024-10-01 01:24:47 +0600

    added multiple routes for different features

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,15 +1,18 @@
-import jwt from "jsonwebtoken";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
 
-const varifyJWT = asyncHandler(async (req, res, next) => {
+export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
-      req.header("Authorization").replace("Bearer ", "");
+      req.header("Authorization")?.replace("Bearer ", "");
 
-    if (!token) return next(new Error("User not logged in"));
+    // console.log(token);
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
@@ -18,15 +21,12 @@ const varifyJWT = asyncHandler(async (req, res, next) => {
     );
 
     if (!user) {
-      throw new ApiError(401, "User not found");
+      throw new ApiError(401, "Invalid Access Token");
     }
 
     req.user = user;
-
     next();
   } catch (error) {
-    throw new ApiError(401, error.message || "Invalid token");
+    throw new ApiError(401, error?.message || "Invalid access token");
   }
 });
-
-export default varifyJWT;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,11 +2,18 @@ import { Router } from "express";
 import {
   loginUser,
   logOutUser,
-  refreshAccessToken,
   registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+  updateAccountDetails,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import varifyJWT from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -23,10 +30,24 @@ router.route("/register").post(
   ]),
   registerUser
 );
+
 router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(varifyJWT, logOutUser);
-router.route("/refreshToken").post(refreshAccessToken);
+router.route("/logout").post(verifyJWT, logOutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
